26e8b210411ff448c0a9113f2524c8a318aa26b0
xiaohuoni <448627663@qq.com>
2020-08-20 15:03:38 +0800

    step: assets-metadata-css metadata

--- config/config.js
+++ config/config.js
@@ -1,3 +1,4 @@
 export default {
     appType: 'h5',
+    title:false,
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
-import { Link } from 'alita';
+import { Link, Helmet } from 'alita';
 
-export default () => <div><Link to="/list">Go to list page</Link></div>;
+export default () => (
+    <div>
+        <Helmet>
+            <title>alita 入门教程</title>
+        </Helmet>
+        <Link to="/list">Go to list page</Link>
+    </div>);
