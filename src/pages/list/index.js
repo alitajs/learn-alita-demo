7092514476bd0bdf1db71ab0d5f70172d05d3566
xiaohuoni <448627663@qq.com>
2020-08-20 10:56:20 +0800

    step: assets-metadata-css assets

--- src/pages/list/index.js
+++ src/pages/list/index.js
@@ -1,6 +1,9 @@
+import LogoImg from '@/assets/logo.png';
+
 export default function List({ history }) {
     return (
         <div>
+            <img src={LogoImg} width={150} />
             <h1>list</h1>
             <button onClick={() => history.goBack()}>go back!</button>
             <button onClick={() => history.push('/')} >go to index!</button>
